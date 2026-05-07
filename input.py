b3a775ff09b0045921e703154c04ef6f023948f0
dmitriy <dimokse@mail.com>
2026-05-07 23:48:02 +0400

    input

---
+++ input.py
@@ -0,0 +1,24 @@
+# #1
+# name = input("Как тебя зовут? ")
+# print(f'Привет, {name}!')
+# #2
+# city = input("В каком городе ты живёш? ")
+# print(f'Ты живёш в городе, {city}!')
+# #3
+# name = input('Твое имя ')
+# last_name = input('Твоя фамилия ')
+# print(f'Полное имя: {name} {last_name}')
+#4
+# age = int(input("Сколько тебе лет? "))
+# print(f"Чрез год тебе будут {age+1}")
+# #5
+# n = int(input("Введите сило "))
+# print(f"Сумма: {n+n}")
+#6
+n = int(input("Первое число "))
+i = int(input("Второе число "))
+print(f'Разность: {n-i}')
+print(f'Произвeдение: {n*i}')
+print(f'Сумма: {n+i}')
+#7
+
