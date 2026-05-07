695f95cc71385d0c9e8bf5307b47e8940b78557c
Dmitriy_B <dimokse@gmail.com>
2026-05-07 22:00:54 +0400

    2

--- print.py
+++ print.py
@@ -1,1 +1,88 @@
-print("Привет мир")
+#1
+print("Привет, Python!")
+#2
+print("Дмитрий")
+#3
+print("Python")
+print("это")
+print("интересно")
+#4
+print(100)
+#5
+print(25+17)
+#6
+name ="Дмитрий"
+age = 25
+print(f'Имя: {name}\nВозраст: {age}')
+#7
+city = "Amsterdam"
+country = "Netherlands"
+print(city, country)
+#8
+print("Python", "3.12")
+#9
+print("07","05","2026", sep="-")
+#10
+print("A","B","C", sep="/")
+#11
+print("hello","word","python", sep=".")
+#12
+print("Python","Is", "Cool", sep="")
+#13
+print("Hello", end=" ")
+print("World")
+#14
+print(1 , end=" ")
+print(2, end= " ")
+print(3, end = " ")
+print(4, end = " ")
+print(5)
+#15
+print("Loading...", "Done!", sep="")
+#16
+name = "Дмитрий"
+language = "Python"
+print(f'{name} изучает {language}')
+#17
+a = 10
+b = 5
+print(f'{a} + {b} = {a+b}')
+#18
+product = "Laptop"
+price = 1200
+print(f'Товар: {product}, цена: {price} евро')
+#19
+product = "Кофе"
+price = 3
+val = 2
+print(f'Товар: {product}\nЦена: {price} евро\nКоличество: {val}\nИтого: {price * val} евро')
+#20
+name = "Dmitry"
+role =  "QA"
+language = "Python"
+print("=== USER ===")
+print(f'Name: {name}\nRole: {role}\nLanguage:{language}')
+print("============")
+
+#21
+print("Name","Age",sep=" ")
+print("Dmitriy", 30)
+print("Anna", 20)
+
+#22
+hours = 8
+rate = 2
+
+print(f'Рабочие часы: {hours}',f'Ставка: {rate}', f'Зарплата: {hours*rate}', sep="\n")
+
+#23
+print("*****", "*****", "*****", sep="\n")
+
+#24
+print("+-----+","|Hello|", "+-----+", sep="\n")
+
+#25
+print(1, "Start", sep=". ")
+print(2, "Settings", sep=". ")
+print(3, "Exit", sep=". ")
+print(f'Choose option:',1)
