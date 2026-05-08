d5f80a6ae632322111ffe093f503460f125d1332
Dmitriy_B <dimokse@gmail.com>
2026-05-08 10:59:02 +0400

    12

--- input.py
+++ input.py
@@ -2,23 +2,44 @@
 # name = input("Как тебя зовут? ")
 # print(f'Привет, {name}!')
 # #2
-# city = input("В каком городе ты живёш? ")
-# print(f'Ты живёш в городе, {city}!')
+# city = input("В каком городе ты живёшь? ")
+# print(f'Ты живёшь в городе, {city}!')
 # #3
+# ya = input("Любимый язык программирования? ")
+# print(f'{ya} - Отличный выбор!')
+#4
 # name = input('Твое имя ')
 # last_name = input('Твоя фамилия ')
 # print(f'Полное имя: {name} {last_name}')
-#4
+#5
+# country = input("В какой стране живёшь? ")
+# city = input("В каком городе живёшь? ")
+# print(f'Ты живёшь в {country}, город {city}')
+#6
 # age = int(input("Сколько тебе лет? "))
 # print(f"Чрез год тебе будут {age+1}")
-# #5
+# #7
 # n = int(input("Введите сило "))
 # print(f"Сумма: {n+n}")
-#6
-n = int(input("Первое число "))
-i = int(input("Второе число "))
-print(f'Разность: {n-i}')
-print(f'Произвeдение: {n*i}')
-print(f'Сумма: {n+i}')
-#7
+#8
+# n = int(input("Первое число "))
+# i = int(input("Второе число "))
+# print(f'Разность: {n-i}', f'Произвeдение: {n*i}',f'Сумма: {n+i}', sep="\n")
+
+#9
+# ch = int(input("Количество рабочих часов: "))
+# st = int(input("Ставка в час: "))
+# print(f'Зарплата: {ch * st} евро')
+#10
+
+# prise = int(input("Цена товара: "))
+# coun = int(input("Количество товара: "))
+# print(f"Итог: {prise * coun} евро")
+#11
+# prise = float(input("Цена товара с копейками: "))
+# print(f"Новая цена: {prise * 2}")
+#12
+h = float(input("Ширина комнаты: "))
+l = float(input("Длинна комнаты: "))
+print(f"Площадь комнаты: {h * l}")
 
