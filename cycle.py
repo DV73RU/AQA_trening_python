93253562a36cf4dc5b2408d24c24c443b5412b4e
Dmitriy_B <dimokse@gmail.com>
2026-05-08 18:03:41 +0400

    cycle.py

---
+++ cycle.py
@@ -0,0 +1,49 @@
+#1
+# for i in range(1,6):
+#     print(i)
+#2
+# for i in range(1,11):
+#     print(i)
+#3
+# for i in range(10,1,-1):
+#     print(i)
+#4
+# for i in range(2,22,2):
+#     print(i)
+#5
+# text = input("Введите слово: ")
+# count = 5
+# for i in range(count):
+#     print(text)
+#6
+# res = 0
+# for i in range(1,10):
+#     res = i+res
+# print(res)
+#7
+# intr = int(input("Ведите число: "))
+# for i in range(1,11):
+#     res = intr * i
+#     print(f"{intr} * {i} = {res}")
+#8
+
+# total = 0
+# for i in range(5):
+#     num = int(input("Введите число: "))
+#     total += num
+# print(f"Сумма {total}")
+
+#9
+# total = 0
+# for i in range(5):
+#     num = int(input("Введите число: "))
+#     if num % 2 == 0:
+#         total = total + 1
+# print(total)
+
+
+#10
+# h = int(input("Ширина: "))
+# l = int(input("Высота: "))
+# for i in range(h):
+#     print("*" * l)
