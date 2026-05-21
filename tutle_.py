b8fc230da4c0472fc31d6e7a50fb4751f722f3d9
Dmitriy_B <dimokse@gmail.com>
2026-05-21 11:10:03 +0400

    30

--- tutle_.py
+++ tutle_.py
@@ -16,42 +16,147 @@
 # print(age)
 # print(city)
 from itertools import count
+from tkinter.font import names
+
+# #1
+# numbers = (5, 10, 15, 20, 25)
+# for i in numbers:
+#     print(i)
+#
+# #2
+# numbers = (3, 7, 10, 2, 8)
+# res = 0
+# for i in numbers:
+#     res = res + i
+# print(f"Сумма: {res}")
+#
+# #3
+# numbers = (4, 7, 12, 9, 20, 3)
+# res = 0
+# counts = 0
+# for i in numbers:
+#     if i % 2 == 0:
+#         counts = counts + 1
+# print(f"Количество чётных чисел кортежа: {counts}")
+#
+# #4
+# numbers = (11, 45, 23, 67, 12)
+# maximum = numbers[0]
+# for i in numbers:
+#     if i > maximum:
+#         maximum = i
+# print(f"Максимально число кортежа: {maximum}")
+#
+# #5
+# names = ("Anna", "Bob", "Tom", "Kate")
+# n = input("Введите имя: ")
+# for i in names:
+#     if i == n:
+#         print("Такое имя есть ")
+#         break
+# else:
+#     print("Такого имени нет")
 
 #1
-numbers = (5, 10, 15, 20, 25)
-for i in numbers:
-    print(i)
+# products = [
+#     ("apple", 50),
+#     ("banana", 30),
+#     ("orange", 70),
+#     ("milk", 100)
+# ]
+#
+# for lst in products: #  Бежим по кортежам в списке
+#     name, prise = lst # Распоковываем кортеж
+#     print(f"{name} стоит {prise}") # Печатаем
 
-#2
-numbers = (3, 7, 10, 2, 8)
-res = 0
-for i in numbers:
-    res = res + i
-print(f"Сумма: {res}")
+#2 найти товар дороже 60
+# products = [
+#     ("apple", 50),
+#     ("banana", 30),
+#     ("orange", 70),
+#     ("milk", 100),
+#     ("bread", 40)
+# ]
+#
+# for lst in products:
+#     name, prise = lst # Распоковываем каждый кортеж
+#     if prise > 60: # Если значение больше 60
+#         print(name) # Выводи их
 
-#3
-numbers = (4, 7, 12, 9, 20, 3)
-res = 0
-counts = 0
-for i in numbers:
-    if i % 2 == 0:
-        counts = counts + 1
-print(f"Количество чётных чисел кортежа: {counts}")
+#3 сумма всех цен
+# products = [
+#     ("apple", 50),
+#     ("banana", 30),
+#     ("orange", 70),
+#     ("milk", 100)
+# ]
+# results = 0
+# for lst in products:
+#     prise = lst[1] # Цена в элементие с инексом 1
+#     results = results + prise # Складываем все цены
+# print(f"Сумма всех товаров: {results}")
+#
+#4 список учеников и оценки
 
-#4
-numbers = (11, 45, 23, 67, 12)
-maximum = numbers[0]
-for i in numbers:
-    if i > maximum:
-        maximum = i
-print(f"Максимально число кортежа: {maximum}")
+# students = [
+#     ("Anna", 5),
+#     ("Bob", 4),
+#     ("Tom", 3),
+#     ("Kate", 5),
+#     ("Max", 2)
+# ]
+#
+# for tuples in students:
+#     name, cou = tuples
+#     if cou == 5:
+#         print(name)
 
-#5
-names = ("Anna", "Bob", "Tom", "Kate")
-n = input("Введите имя: ")
-for i in names:
-    if i == n:
-        print("Такое имя есть ")
-        break
-else:
-    print("Такого имени нет")
+#5 создать новый список из кортежей
+
+# names = ["Anna", "Bob", "Tom"]
+# ages = [20, 25, 30]
+#
+# res = []
+#
+# for i in range(len(names)):
+#     # for j in range(len(ages)):
+#     tup = (names[i],ages[i])
+#     res.append(tup)
+# print(res)
+
+#6. Нужно создать новый список кортежей:
+
+# products = ["apple", "banana", "milk", "bread"]
+# prices = [50, 30, 100, 40]
+#
+# res = []
+# for i in range(len(products)):
+#     tub = (products[i], prices[i])
+#     res.append(tub)
+# print(res)
+
+# products = ["apple", "banana", "milk", "bread"]
+# prices = [50, 30, 100, 40]
+#
+# res = []
+
+#Вариант через enumerate
+# for i, name in enumerate(products):
+#     tup = (name,prices[i])
+#     res.append(tup)
+# print(res)
+#
+# for i in range(len(products)): # Получаем индексы
+#
+#     if prices[i] > 40: # Перебираем все цены больше 40
+#         tup = (products[i], prices[i])
+#         res.append(tup) #Добавляем в список кортеж
+# print(res)
+
+#8 Нужно создать список кортежей только для товаров, у которых общая стоимость больше 100.
+products = ["apple", "banana", "milk", "bread", "cheese"]
+prices = [50, 30, 100, 40, 150]
+counts = [3, 5, 2, 4, 1]
+
+
+for i in range(len(products)):
