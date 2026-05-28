977ecf57e68fa9d6f4b020d34af08fe586ead941
Dmitriy_B <dimokse@gmail.com>
2026-05-28 15:07:45 +0400

    except_.py

---
+++ f_int.py
@@ -0,0 +1,21 @@
+# a = 10
+# b = 3
+# n= -15
+# print(abs(n))
+# x = 3.14159
+#
+# print(round(x))
+# print(round(x,2))
+#
+# n = int(input("ВВЕДИТЕ ЧИСЛО: "))
+# s = pow(n,2)
+# print(s)
+
+name = "Анна"
+age = 28
+print(f"{name} - {age}")
+
+pi = 3.123345
+print(f"{round(pi,2)}")
+print(f"{pi:.2f}")
+print(f"{pi:.3f}")
