6f8d664f1b6bfe2b94503ecee786eac5014a4ee9
dmitriy <dimokse@mail.com>
2026-06-04 18:31:41 +0400

    file_pathlib.py

---
+++ logs/per.py
@@ -0,0 +1,29 @@
+my_numbers = [2,1,3,4]
+your_numbers = [2,1,3,4]
+my_numbers_agen = my_numbers
+# print(my_numbers == your_numbers)
+# print(my_numbers is your_numbers)
+# my_numbers[0] = 100
+# print(my_numbers)
+# print(id(my_numbers))
+
+# print(id(my_numbers))
+# print(id(your_numbers))
+
+# d = 1
+# s = 0
+# r = s
+print(f"my_numbers - {my_numbers}")
+print(f"your_numbers - {your_numbers}")
+print(my_numbers == your_numbers)
+# print(d in s)
+print(f"my_numbers- {id(my_numbers)}, your_numbers- {id(your_numbers)}, my_numbers_agen - {id(my_numbers_agen)}")
+print("---------------------------------")
+
+# s = s + 1
+# print(f"s - {s}")
+# print(f"r - {r}")
+# print(s == d == r)
+# print(f"d-{id(d)}, s- {id(s)} r - {id(r)}")
+
+
--- tasks_100.py
+++ tasks_100.py
@@ -1,8 +1,6 @@
-x,y,z = int(input()),int(input()),int(input())
-ls = [x,y,z]
-print(ls)
-res = 0
-for i in ls:
-    if i > 0:
-        res += res
+n = int(input())
+res = 1
+for i in range(1,n+1):
+    print(i)
+    res = res *i
 print(res)
