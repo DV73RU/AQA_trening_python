e84b209de63921c0626c2664aab0b073cc9894ef
dmitriy <dimokse@mail.com>
2026-06-16 18:40:07 +0400

    list

--- TO-DO LIST/todo.py
+++ TO-DO LIST/todo.py
@@ -1,15 +1,44 @@
 import json
 
-Вернулся к банкомату: Добавил
 
-```
 
 def show_menu():
-    print("+-------------------------------------+")
-    print("!         МЕНЮ ДОСТУПНЫХ ОПЕРАЦИЙ     !")
-    print("+-------------------------------------+")
-    print("!  [Задачи] Показать все задачи       !")
-    print("!  [Добавить]  Добавить задачу        !")
-    print("!  [Готово]  Отметить выполненой по id!")
-    print("!  [Выход]  Выход                     !")
-    print("+-------------------------------------+")
+    print("+--------------------------------------+")
+    print("!         МЕНЮ ДОСТУПНЫХ ОПЕРАЦИЙ      !")
+    print("+--------------------------------------+")
+    print("!  [Список] Показать все задачи        !")
+    print("!  [Добавить]  Добавить задачу         !")
+    print("!  [Готово]  Отметить выполненой по id !")
+    print("!  [Выход]  Выход                      !")
+    print("+--------------------------------------+")
+
+
+show_menu()
+
+with open("todo.json","r",encoding="utf-8") as file:
+    data = json.load(file)
+
+# print(data)
+# print(json.dumps(data,indent=1,ensure_ascii=False))
+
+list_task = "Список"
+set_done = "Готово"
+
+done_false = "[ ]"
+done_true = "[✓]"
+
+while True:
+    n = input("Введите комаеду: ")
+    if n == list_task:
+        for i in data['tasks']:
+            if i.get('done') is False:
+                print(f"{i['id']}. {done_false} {i['task']}")
+            elif i.get('done') is True:
+                print(f"{i['id']}. {done_true} {i['task']}")
+    elif n == set_done:
+        # Добавить если нет id
+        _id = int(input("Введите id задачи: "))
+        for i in data['tasks']:
+            if i.get('id') == _id:
+                print(f"{i['id']}. {done_false} {i['task']}")
+
