bc9704ea5b7de0e71c842580428c4b01b6547e1d
Dmitriy_B <dimokse@gmail.com>
2026-06-17 11:41:05 +0400

    done

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,6 +1,32 @@
 import json
 
-with open("quiz.py","r",encoding="utf-8") as file:
+with open("quiz.json","r",encoding="utf-8") as file:
     data = json.load(file)
 
-print(json.dump(data,file,indent=4,ensure_ascii=False))
+
+# print(data)
+
+# print(json.dumps(data,ensure_ascii=False,indent=4))
+
+count_q = 0
+total = len(data["questions"])
+
+for num,val in enumerate(data["questions"],start=1):
+
+    question = val['question']
+    options =  val['options']
+    answer = val['answer']
+
+    print(f"Вопрос {num}: {question}")
+    for idx,otv in enumerate(options,start=1):
+        print(f'{idx}. {otv}')
+    n = input("Ваш ответ: ")
+
+    if n == answer:
+        count_q = count_q + 1
+        print("Верно! ✓")
+
+    else:
+        print(f"Не верно! Правильный ответ :{answer}")
+
+print(f"Результат: {count_q} из {total}")
