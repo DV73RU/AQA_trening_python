08ea3ea70c4d792febc327f24d72fab729338149
dmitriy <dimokse@mail.com>
2026-06-18 17:57:05 +0400

    done

--- grades/grades.py
+++ grades/grades.py
@@ -1,12 +1,43 @@
 import json
 import os
 
+samples = {
+    "students": [
+        {"name": "---", "grades": []},
+        {"name": "---", "grades": []}
+    ]
+}
+
 def clear_consol():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 path_file = "grades.json"
-with open(path_file, "r", encoding="utf-8") as f:
-    data = json.load(f)
+
+try:
+    with open(path_file, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    print("Файл успешно открыт и прочитан")
+    is_json = True
+except FileNotFoundError:
+    print("Файл не найден!")
+    is_json = False
+
+except json.JSONDecodeError:
+    is_json = False
+    print("Файл не JSON или повреждён")
+
+if not is_json:
+    js = input("Создать файл ? Да/Нет: ")
+    if js == "Да":
+        with open(path_file,"w", encoding="utf-8") as file:
+            json.dump(samples,file,ensure_ascii=False,indent=4)
+        with open(path_file,"r",encoding="utf-8") as file:
+            data = json.load(file)
+    else:
+        print("Доствидания")
+        exit()
+
+
 
 def sow_menu():
     menu_items = {
@@ -18,7 +49,7 @@ def sow_menu():
     }
 
     print("+--------------------------------------+")
-    print("!         МЕНЮ ДОСТУПНЫХ ОПЕРАЦИЙ      !")
+    print("!         МЕНЮ ДОСТУПНЫХ КОМАНЛ        !")
     print("+--------------------------------------+")
     for cmd, desc in menu_items.items():
         # Форматируем строку: левая часть шириной 10 символов, правая — в остаток
@@ -26,7 +57,7 @@ def sow_menu():
         print(f"! {text.ljust(36)} !")
     print("+--------------------------------------+")
 
-def write_file(file): #Функция пишет в файл
+def write_file(): #Функция пишет в файл
     with open(path_file, "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
@@ -67,7 +98,12 @@ while True:
             name = stud['name']
             grades = stud['grades']
             res = sum(grades)
-            average = res / len(grades)
+            try:
+                average = res / len(grades)
+            except ZeroDivisionError:
+                # print("Оценок нет")
+                average = "Нет оценок"
+
             print(f"{num}. {name + (' ' *(7-len(name)))} | оцентка:{grades} | средний бал: {average}")
 
 
@@ -85,14 +121,14 @@ while True:
                 int_grade = int(input("Оценка: "))
                 grades.append(int_grade)
 
-                write_file(path_file)
+                write_file()
 
         if not found:
             print(f"Нет такого имени {int_name}, создаю нвую запись")
             int_grade = int(input("Оценка: "))# Если есть такое имя добавь ему оценку, если нет создай новое имя и добавь оценку
             data['students'].append({"name":int_name,"grades":[int_grade]})
 
-            write_file(path_file)
+            write_file()
 
 
     else:
