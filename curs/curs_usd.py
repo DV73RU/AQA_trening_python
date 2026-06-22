7cc245d02f4cc6407672b40965cffd3d64aa2556
Dmitriy_B <dimokse@gmail.com>
2026-06-22 16:40:29 +0400

    done

---
+++ curs/curs_usd.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+url = 'https://open.er-api.com/v6/latest/USD'
+
+respons = requests.get(url)
+data = respons.json()
+print(json.dumps(data,ensure_ascii=False,indent=4))
+
+in_val = int(input("Сумма USD: "))
+valut = input("Валюта: ")
+res = 0
+for vl, value  in data['rates'].items():
+    # print(f"Валюта {vl} - Курс {value}")
+    if valut == vl:
+        new_value = float(value)
+        res = value * in_val
+        print(f"{in_val} USD = {res} {valut}")
