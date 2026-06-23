788d06bef2111bc1dc33b56a2331a4a4ffda7d00
Dmitriy_B <dimokse@gmail.com>
2026-06-23 11:39:25 +0400

    Add Wikipedia.py

---
+++  Wikipedia/Wikipedia.py
@@ -0,0 +1,27 @@
+import requests
+
+url = "https://ru.wikipedia.org/api/rest_v1/page/summary/Python"
+
+headers = {
+    "User-Agent": "MyPythonApp/1.0 (study project)"  # ← только латиница
+}
+
+proxies = {
+    "http": "http://iXymu9PdPw:Jc6LOE7vTj1@89.127.214.80:10080",
+    "https": "http://iXymu9PdPw:Jc6LOE7vTj@89.127.214.80:10080"
+}
+
+try:
+    response = requests.get(url, headers=headers, proxies=proxies, timeout=10)
+    print(f"Статус: {response.status_code}")
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data['extract'])
+    else:
+        print("Статья не найдена!")
+
+except requests.exceptions.RequestException as e:
+    print("Ошибка запроса:", e)
+finally:
+    print("Скрипт завершён")
