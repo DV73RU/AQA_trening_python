05222ac89e7f7559c7282e34e37b00c26bce56da
dmitriy <dimokse@mail.com>
2026-06-26 15:32:22 +0400

    fix bug

--- class/inheritance/Product.py
+++ class/inheritance/Product.py
@@ -9,8 +9,15 @@ class Product:
 
     def info(self):
         """Метод класса информация о продукте"""
-        print(f"{self.name} | Цена: {self.price} | Количество {self.quantity}",end="")
+        print(f"{self.name} | Цена: {self.price} | Количество: {self.quantity}",end="")
 
+    def buy(self,count):
+        """Метод покупки продукта"""
+        if self.quantity > 0:
+            self.quantity = self.quantity - count
+            print(f"Куплено: {count} шт. {self.name} Осталось {self.quantity}")
+        else:
+            print(f"Купить нельзя! {self.name} - {self.quantity} шт.")
 
 class Food(Product):
     """Класс еда"""
@@ -23,9 +30,54 @@ class Food(Product):
 
         data_object = datetime.strptime(self.expiry_date,"%Y-%m-%d").date() #Из строки в дату
         print(f" | Срок до: {data_object}") # К базовой info добавили параметр
-        
+
+    def is_expired(self):
+        today = date.today()
+        data_object = datetime.strptime(self.expiry_date, "%Y-%m-%d").date()
+        if today > data_object:
+            print(f"{self.name} просрочен!")
+        else:
+            print(f"{self.name} свежий, срок до: {data_object}")
+
+class Electronics(Product):
+    """Класс электроника"""
+    def __init__(self,name,price,expiry_date,warranty):
+        super().__init__(name,price,expiry_date)
+        self.warranty = warranty
+    def info(self):
+        super().info()
+        print(f" | Гарантия: {self.warranty}")
+    def claim_warranty(self):
+        print(f"{self.name} | Гарантия {self.warranty} мес.")
 
 
+class Clothing(Product):
+    def __init__(self,name,price,quantity,size):
+        super().__init__(name,price,quantity)
+        self.size = size
 
-banan = Food("Бананы",100,10,"2026-11-01")
+
+    def try_on(self):
+        print(f"Примеряете {self.name} | Размер: {self.size}")
+
+banan = Food("Бананы",10,10,"2026-11-01")
+telef = Electronics("Samsung",1000,2,24)
+
+shirt = Clothing("Рубашка", 2000, 50, "L")
+
 banan.info()
+telef.info()
+
+
+telef.claim_warranty()
+
+banan.buy(5)
+banan.info()
+banan.buy(5)
+banan.info()
+banan.buy(5)
+banan.info()
+banan = Food("Бананы",10,10,"2026-11-01")
+banan.buy(10)
+
+shirt.try_on()
