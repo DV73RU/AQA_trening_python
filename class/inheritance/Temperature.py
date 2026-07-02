548434b38e65f49fccf8ed667f0a0329784614b1
dmitriy <dimokse@mail.com>
2026-07-02 11:07:13 +0400

    fix bug

--- class/inheritance/Temperature.py
+++ class/inheritance/Temperature.py
@@ -1,21 +1,37 @@
 class Temperature:
     """Класс родитель температура"""
-    def __init__(self,temp_celsius): #Конструктор класса принимает атрибуты.
-        # c = 1.8
+    def __init__(self,temp_celsius): #Конструктор класса принимает атрибуты температуру в цельсиях.
         self.temp_celsius = temp_celsius
 
-    def fahrenheit(self): #Метод вернёт температуру в фарингейте
-        return self.temp_celsius * 1.8 + 32
 
-    def kelvin(self): #Метод вернёт температуру в Кельвинах
-        pass
-
-    def grad_nuton(self): #Метод вернёт температуру в градусах Ньютона
-        pass
-
-    # @classmethod
-    # def set_
+    def info(self):# Метод экземпляра
+        print(f"Температура : {self.temp_celsius}°C" )
 
 
-cel = Temperature(10)
-print(cel.fahrenheit())
+    @staticmethod # Декоратор статического метода
+    def to_fahrenheit(c): #Метод статический: Конвертирует в из цельсия в фарингейты, принимает цельсии
+        f = c *(9/5) + 32
+        print(f"{f} F" )
+
+    @staticmethod # Декоратор статического метода
+    def to_kelvin(c):# Метод принимает целсии вернет кельфины
+        k = c + 237.15
+        print(f"{k} K")
+
+    @classmethod # Декоратор метода класса
+    def from_fahrenheit(cls,f): #Метод принимает фаренгейты вернёт Цельсии
+        c = (f - 32) * 5/9
+        return cls(c)
+
+
+
+
+t = Temperature(10) #
+t.info()
+
+Temperature.to_fahrenheit(100) # Вызываем класс с методом конвертации в фаренгейты
+Temperature.to_kelvin(100)
+
+
+t2 = Temperature.from_fahrenheit(100)
+t2.info()
