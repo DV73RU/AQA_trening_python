33d56362bee50c0077031851bacf8c72511ae8bb
dmitriy <dimokse@mail.com>
2026-07-03 15:01:30 +0400

    fix

--- class/inheritance/Stack.py
+++ class/inheritance/Stack.py
@@ -7,23 +7,41 @@ class Stack:
         """Весть стек"""
         print(f"Стек: {self.items}")
 
-    def dequeue(self):
+    def peek(self):
+        """Смотрим вершина стека (последенего в очереде) не забирая из неё"""
         if len(self.items) == 0:
-            print("Пусто в очереди")
+            print("Пусто в стеке")
         else:
-            print(f"{self.items.pop(0)} вышел из очереди")
+            print(f"Вершина :{self.items[-1]}")
 
     def size(self):
         """Длина стека"""
         print(f"Размер: {len(self.items)}")
 
-    def peek(self):
+    def pop(self):
+        """Удалить с вершины стека (последнего в списке)"""
         if len(self.items) == 0:
-            print("Пусто в очереди")
+            print("Пусто в стеке")
         else:
-            print(f"Первый {self.items[0]}")
+            print(f"{self.items.pop(-1)} убран с вершины")
 
     def push(self,name:str):
         """Добавить на вершину стека (последний в списке)"""
         self.items.append(name)
 
+    def is_empty(self):
+        """Проверка пустоты списка"""
+        if len(self.items) == 0:
+            print(f"Стек пустой")
+        else:
+            print("Стек с элементами")
+
+v = Stack()
+v.info()
+v.is_empty()
+v.push("Толя")
+v.push("Коля")
+v.info()
+
+v.peek()
+v.is_empty()
