3dad78b32d32dfc6ad778ff1c37fa5ac7907c4cb
dmitriy <dimokse@mail.com>
2026-07-08 13:55:03 +0400

    add изменение количество обслуживаний

--- class/inheritance/restoran.py
+++ class/inheritance/restoran.py
@@ -3,14 +3,22 @@ class Restaurant:
     def __init__(self,res_name,cuisine_type):
         self.res_name = res_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0 #<- Количество обслуженных посетителей
+
 
     def describe_restaurant(self):
         """Метод выводит два атрибута"""
         print(f"{self.res_name} | Кухня: {self.cuisine_type}")
+        print(f"Всего обслужено посетителей: {self.number_served}")
 
     def open_restaurant(self):
         print(f"Ресторан {self.res_name} - Открыт")
 
+    def set_number_served(self,count):
+        if self.number_served <= count:
+            self.number_served = self.number_served + count
+        else:
+            print("Отрицательное число не доступно")
 class Users:
     """Класс пользователи"""
 
@@ -26,6 +34,8 @@ class Users:
 
     def greet_user(self):
         print(f"Приветствую вас {self.first_name} у нас")
+
+
 
 rest = Restaurant("У Палыча","Кавказская")
 rest1 = Restaurant("Три корочки","Русская")
@@ -49,3 +59,14 @@ rest.describe_restaurant()
 # rest.open_restaurant()
 rest1.describe_restaurant()
 rest2.describe_restaurant()
+
+
+rest3 = Restaurant("Шаурмичка","Восточная")
+print(rest3.number_served)
+rest3.number_served = 10
+print(rest3.number_served)
+
+# rest3.set_number_served(10)
+rest3.describe_restaurant()
+rest3.set_number_served(23)
+rest3.describe_restaurant()
