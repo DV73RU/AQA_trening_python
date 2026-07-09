b99e88cfcafc9728feafd67743bd47f53506b5b6
dmitriy <dimokse@mail.com>
2026-07-09 16:25:47 +0400

    add Device.py

--- class/inheritance/Device.py
+++ class/inheritance/Device.py
@@ -1,3 +1,6 @@
+from orca.orca_state import device
+
+
 class Device:
     """Класс устройство"""
     def __init__(self,name,status=False):
@@ -14,7 +17,35 @@ class Device:
 
     def info(self):
         """Метод выводит информацию о состоянии устройства"""
-        if self.status == False:
-            print(f"Устройство {self.name} - Выключено 🔴 ")
+        if not self.status:
+            print(f"    Устройство {self.name} - Выключено 🔴 ")
         else:
-            print(f"Устройство {self.name} - Включено 🔵 ")
+            print(f"    Устройство {self.name} - Включено 🔵 ")
+
+class Room:
+    """Класс комнаты"""
+    def __init__(self,name):
+        self.name = name
+        self.device = []
+
+    def add_device(self,device):
+        """Метод добавляет устройство в комнату"""
+        self.device.append(device)
+        print(f"В комнату {self.name} добавлено устройство {device.name}")
+
+    def torn_on_all(self):
+        """"Метод включает все устройства комнате"""
+        pass
+
+    def info(self):
+        """Метод выводит информацию о комнате"""
+        print(f"{self.name}")
+        for dev in self.device:
+            print(f"{dev.name})
+
+lamp = Device("Лампа")
+gostin = Room("Гостиная")
+
+gostin.add_device(lamp)
+
+gostin.info()
