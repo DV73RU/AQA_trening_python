a979f308d74d81d94483914a26de0ecb57e5817b
dmitriy <dimokse@mail.com>
2026-07-16 17:35:11 +0400

    add Notification.py

---
+++ class/inheritance/Notification.py
@@ -0,0 +1,51 @@
+class Notification:
+    list_notification = []
+
+
+    def __init__(self,title, message):
+        self.title = title
+        self.message = message
+        self.list_notification.append(self) #< - В список добавляем объект
+
+    def info(self):
+        pass
+
+    def send(self):
+        for i in self.list_notification:
+            i.send
+
+class EmailNotification(Notification):
+    def __init__(self,title,message,mail):
+        super().__init__(title,message)
+        self.mail = mail
+
+    def send(self):
+        print(f" 📧 Email {self.mail} | {self.title}: {self.message} ")
+
+class SMSNotification(Notification):
+    def __init__(self,title,message,phone):
+        super().__init__(title,message)
+        self.phone = phone
+
+    def send(self):
+        print(f" 📱 {self.phone} | {self.title}: {self.message} ")
+
+class  PushNotification(Notification):
+    def __init__(self,title, message,devise_id):
+        super().__init__(title,message)
+        self.devise_id = devise_id
+
+    def send(self):
+        print(f" Push {self.devise_id} | {self.title}: {self.message}" )
+
+
+email = EmailNotification("Заказ принят","Заказ №123 принят", "mail@mail.ru")
+email.send()
+sms = SMSNotification("Код: 1234","Не кому не сообщайте код","+70000000000")
+sms.send()
+
+push = PushNotification("Акция","ВНИМАНИЕ СКИДКА 10%", "devise_dsre23fd")
+push.send()
+
+for item in Notification.list_notification:
+    item.send()
