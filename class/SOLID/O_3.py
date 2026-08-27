d243c070ddcd49b95c05b14aade9efce57f718f3
Dmitriy_B <dimokse@gmail.com>
2026-08-27 18:47:32 +0400

    fix

--- class/SOLID/O_3.py
+++ class/SOLID/O_3.py
@@ -17,3 +17,48 @@ CartPage.
 from abc import ABC, abstractmethod
 
 
+class BasePage(ABC):
+    def __init__(self, url: str, title: str):
+        self.url = url
+        self.title = title
+
+    @abstractmethod
+    def check_page(self) -> bool:  # <- Метод проверки страницы
+        pass
+
+
+class LoginPage(BasePage):
+    def check_page(self) -> bool:
+        return self.url.endswith('login.ru') and self.title == "Вход"
+
+
+class CatalogPage(BasePage):
+    def check_page(self) -> bool:
+        return self.url.endswith('catalog.ru') and self.title == "Каталог"
+
+
+class CartPage(BasePage):
+    def check_page(self) -> bool:
+        return self.url.endswith('cart.ru') and self.title == "Корзина"
+
+
+def check_page(page: BasePage) -> str:
+    result = page.check_page()
+    return result
+    # status = "Пройдена" if result else "Ошибка"  # Если метод вернул False
+    #
+    # return (
+    #     f"--- Проверка класса {page.__class__.__name__} ---\n"
+    #     f"URL: {page.url}\n"
+    #     f"Title: {page.title}\n"
+    #     f"Результат проверки: {status} ({result})\n"
+    # )
+
+
+login_page = LoginPage('login.ru', "Авторизация")
+catalog_page = CatalogPage('catalog.ru', "Каталог")
+cart_page = CartPage("cart.ru", "Корзина")
+
+print(check_page(login_page))
+print(check_page(catalog_page))
+print(check_page(cart_page))
